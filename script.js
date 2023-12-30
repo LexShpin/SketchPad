4d1c514f58de5eca4263b353a1ad894af8b2ba50
lexshpin <lexshpin@gmail.com>
2023-12-30 06:00:21 -0500

    Added grid and coloring

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('hello, world!')
+
+const grid = document.querySelector('.grid')
+let gridItems = document.querySelectorAll('.grid__item')
+
+let colorInput = document.getElementById('color-input')
+
+const createGridItem = (size) => {
+    const gridItem = document.createElement('div')
+    gridItem.classList.add('grid__item')
+
+    let gridItemWidth = 100 / size
+
+    gridItem.style.width = `${gridItemWidth}%`
+    gridItem.style.height = `${gridItemWidth}%`
+
+    grid.appendChild(gridItem)
+}
+
+const generateGrid = (size) => {
+    for (let i = 1; i <= size; i++) {
+
+        for (let j = 1; j <= size -1; j++) {
+            createGridItem(size)
+        }
+        createGridItem(size)
+    }
+
+    gridItems = document.querySelectorAll('.grid__item')
+}
+
+generateGrid(5)
+
+gridItems.forEach(item => item.addEventListener('mouseenter', e => {
+    item.style.backgroundColor = colorInput.value
+}))
+
